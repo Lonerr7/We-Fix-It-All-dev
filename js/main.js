ebc9c17a9449da6199353d9fd8d5b4fb1c01db78
Lonerr7 <krabik2342@gmail.com>
2021-11-10 20:21:15 +0300

    First commit. Made header__menu with submenu for desktop screens

---
+++ js/main.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// Menu-burger
+const body = document.querySelector('body');
+const menuArrows = document.querySelectorAll('.menu__arrow');
+const mobileMenuArrows = document.querySelectorAll('.mobile-menu__arrowmobile-menu__arrow');
+const menuListSublist = document.querySelector('.menu__list-item--sublist');
+const menuSublist = document.querySelector('.menu__sublist');
+
+const isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry()||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows()
+        )
+    }
+};
+
+if (isMobile.any()) {
+    body.classList.add('_touch');
+} else {
+    body.classList.add('_pc');
+}
+
+function showSubMenu(selector, className) {
+    if (selector.length > 0) {
+        selector.forEach(arrow => {
+            arrow.addEventListener('click', () => {
+                arrow.parentElement.classList.toggle(className);
+            })
+        });
+    }
+}
+
+showSubMenu(menuArrows, 'menu__list-item--active');
+showSubMenu(mobileMenuArrows, 'mobile-menu__list-item--active');
